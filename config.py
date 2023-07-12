a419d1b7acdd74edd0aba93d1ec6c8319d89fd7a
Trippy <138500712+Trippy07@users.noreply.github.com>
2023-07-12 15:23:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -61,7 +61,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(5493260414)
+ADMINS.append(2136843654)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
