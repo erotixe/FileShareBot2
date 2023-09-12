cc56de15d9c2d21f82a8e8325bc25f9f2219754f
Trippy <138500712+Trippy07@users.noreply.github.com>
2023-09-12 17:40:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 #start message
 START_MSG = os.environ.get("START_MESSAGE", "<b>Hello {first}\n\nI am a file store bot Powered by @Animes_xyz</b>.")
 try:
-    ADMINS=[6158987073]
-    for x in (os.environ.get("ADMINS", "5753413060").split()):
+    ADMINS=[]
+    for x in (os.environ.get("ADMINS", "5086525318").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
