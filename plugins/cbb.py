6772d9060e872ee91dc6c35a2e95af3aac5b56b8
Trippy <138500712+Trippy07@users.noreply.github.com>
2023-10-12 04:12:46 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={5493260414}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Channel : @Animes_xyz\n○ Support Group : @Animetalks0</b>",
+            text = f"<b>╔════════════⦿\n├⋗ ᴄʀᴇᴀᴛᴏʀ : <a href='tg://user?id={5493260414}'>⚚ 𝐓𝐑𝐈𝐏𝐏𝐘 ❄️ </a>\n├⋗ ʟᴀɴɢᴜᴀɢᴇ : <code>Python3</code>\n├⋗ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n├⋗ ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href=https://t.me/Trippy_Xt>File Store Bot</a>\n├⋗ Main Channel : <a href=https://t.me/Animes_Xyz>Anime Xyz</a>\n├⋗ Support Group : <a href=https://t.me/Animetalks0>Anime Talks</a>\n╚═════════════════⦿</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
