ed86bac8143609896a36de6b2690bd5908f10c1b
Trippy <138500712+Trippy07@users.noreply.github.com>
2023-10-12 15:27:01 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,7 +88,7 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
     [
-        InlineKeyboardButton("Join Channel", url="https://t.me/Animes_Xyz"),
+        InlineKeyboardButton("⚡ 𝖩𝗈𝗂𝗇 𝖢𝗁𝖺𝗇𝗇𝖾𝗅 ⚡", url="https://t.me/Animes_Xyz"),
     ],
     [
                     InlineKeyboardButton("⚔️ About Me ⚔️", callback_data = "about"),
