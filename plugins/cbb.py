a9c0187660d1ad73fd26348a3343628b97f056a4
kaustubh090 <126164591+kaustubh090@users.noreply.github.com>
2024-01-02 13:59:40 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>╔════════════⦿\n├⋗ ᴄʀᴇᴀᴛᴏʀ : <a href='tg://user?id={5493260414}'>⚚ 𝐓𝐑𝐈𝐏𝐏𝐘 ❄️ </a>\n├⋗ ʟᴀɴɢᴜᴀɢᴇ : <code>Python3</code>\n├⋗ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n├⋗ ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href=https://t.me/Trippy_Xt>File Store Bot</a>\n├⋗ Main Channel : <a href=https://t.me/Animes_Xyz>Anime Xyz</a>\n├⋗ Support Group : <a href=https://t.me/Animetalks0>Anime Talks</a>\n╚═════════════════⦿</b>",
+            text = f"<b>\n○  ᴄʀᴇᴀᴛᴏʀ : <a href='tg://user?id={5191566338}'>Killua </a>\n○  ʟᴀɴɢᴜᴀɢᴇ : <code>Eng Sub & Dub</code>\n○  Main Channel : <a href=https://t.me/Animes_Alliance>Anime Alliance</a>\n○  Support Group : <a href=https://t.me/Alliance_Weebs>Anime Chats</a>\n</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
