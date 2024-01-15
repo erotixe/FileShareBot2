a94334957ded9bfefd66787ad3a0af394277424d
ᴀsʜ <153292001+erotixe@users.noreply.github.com>
2024-01-15 15:52:14 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -15,7 +15,7 @@ from config import ADMINS, OWNER_ID, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISAB
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
 
-
+SECONDS = int(os.getenv("SECONDS", "10"))
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
