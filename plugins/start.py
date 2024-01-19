a33c33ef9a47fcd45de50c9f24596c7c03f3c24c
ᴀsʜ <153292001+erotixe@users.noreply.github.com>
2024-01-19 20:57:05 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -78,6 +78,11 @@ async def start_command(client: Client, message: Message):
                 reply_markup = None
 
             try:
+              await message.reply_text(
+            f"Files will be deleted in 15 minutes to avoid copyright issues. Please forward and save them.",
+            disable_web_page_preview=True,
+            quote=True
+              )
                 snt_msg = await msg.copy(chat_id=message.from_user.id, caption=caption, parse_mode=ParseMode.HTML, reply_markup=reply_markup,
                                           protect_content=PROTECT_CONTENT)
                 await asyncio.sleep(0.5)
